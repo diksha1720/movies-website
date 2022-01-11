1810abc8485930c26a577054c9bd65f6e16f648f
Diksha1720 <dikshabharti39@gmail.com>
2022-01-11 10:28:03 +0530

    added functionality to update the existing info

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+
+MOVIE_API_KEY = 'ecd118040b987ede4e895eb1338fb069'
+MOVIE_ENDPOINT = f"https://api.themoviedb.org/3/search/movie"
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
+
 
 ##CREATE DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
@@ -22,9 +27,9 @@ class Movie(db.Model):
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    rating = db.Column(db.Float, nullable=True)
-    ranking = db.Column(db.Integer, nullable=True)
-    review = db.Column(db.String(250), nullable=True)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
 # db.create_all()
@@ -89,8 +94,37 @@ def add():
     movieform = Addmovie()
     if movieform.validate_on_submit():
         new_movie = movieform.movie_name.data
+        response = requests.get(MOVIE_ENDPOINT, params={"api_key": MOVIE_API_KEY, "query": new_movie})
+        # data = response.json()['results'][0]['original_title']
+        data = response.json()['results']
+        return render_template('select.html', data=data)
+    else:
+        return render_template('add.html', form=movieform)
 
-    return render_template('add.html', form=movieform)
+
+@app.route('/get_details/<id>')
+def get_details(id):
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{id}", params={"api_key": MOVIE_API_KEY}).json()
+    title = response['original_title']
+    year = response['release_date'].split('-')[0]
+    desc = response['overview']
+    rating = response['vote_average']
+    img = response['poster_path']
+    img_url = f'https://image.tmdb.org/t/p/original{img}'
+    new_movie = Movie(
+        title=title,
+        year=year,
+        description=desc,
+        rating=rating,
+        img_url=img_url
+    )
+    db.session.add(new_movie)
+    db.session.commit()
+    movie = Movie.query.filter_by(title=title).first()
+    id = movie.id
+    movie.ranking = id
+    db.session.commit()
+    return redirect(url_for('edit', id=id))
 
 
 if __name__ == '__main__':
